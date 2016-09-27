967c71561c53aed2c5b48d85291cfa7ad2d3f140
saveyou <rangga.sidiq@gmail.com>
2016-09-27 23:59:05 +0700

    Get past content randomly and post it again

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # AutoRebuffer
 
-Republish published buffer updates during certain timespan randomly.
+Steps:
+1. Create Account at https://buffer.com/developers/
+2. Rename config.rb.sample and fill with your credential
+3. Run bundle install
+4. Set cron e.g per 8 AM and 13 PM = 0 8,13 * * * /usr/local/ruby /your_parent_dir/publisher.rb
+5. Run rvm cron setup if you're using RVM
---
+++ publisher.rb
@@ -0,0 +1,36 @@
+require 'buffer'
+require './config'
+
+client  = Buffer::Client.new(BUFFER_ACCESS_TOKEN)
+updates = []
+loop    = 2
+processing  = true
+lists   = []
+
+while loop <= 10 && processing == true
+  updates[loop] = client.updates_by_profile_id(BUFFER_PROFILE_ID, {status: 'sent', page: loop, count: 20, since: 1470640359})
+  p "Processing Page"+loop
+  updates[loop].updates.each do |u|
+    if u.media && u.media.link
+      lists.push({text:u.text,link:u.media.link})
+    end
+  end
+  processing = false if !updates[loop].updates[0]
+  loop += 1
+end
+
+if lists
+  rand  = Random.new.rand(lists.count)
+  new_post = lists[rand]
+  client.create_update(
+      body: {
+          text: new_post[:text],
+          profile_ids: [BUFFER_PROFILE_ID],
+          media: {link: new_post[:link]},
+          profile_service: 'facebook',
+          now: 1
+      }
+  )
+end
+
+p "Done"
